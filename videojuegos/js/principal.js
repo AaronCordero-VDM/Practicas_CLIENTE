aecd6acc50650ad0ad54bedfe0ebfe2e1a4b506f
AaronCordero-VDM <aaronciempo@gmail.com>
2023-11-08 12:16:16 +0100

    08/11

---
+++ videojuegos/js/principal.js
@@ -0,0 +1,6 @@
+onload=principal;
+
+function principal(){
+    //concectarse por ajax al servidor, pedir os datos de videojuegos y rellenar la tabla,
+    //con etiqueta <template> crea una estructura sin usarla en el html y asi poderla rellenarla con datos
+}
